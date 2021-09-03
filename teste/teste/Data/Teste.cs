70a1c188efc0bcf70725e4081731831297682347
Pedro Gomes <aluno21279@ipt.pt>
2021-09-03 17:05:17 +0100

    Criação dos modelos e da database

---
+++ teste/teste/Data/Teste.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UniqueDrinks.Models;
+
+namespace teste.Data
+{
+    public class Teste : IdentityDbContext
+    {
+        public Teste(DbContextOptions<Teste> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Bebidas> Bebidas { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<Imagens> Imagens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Reservas>()
+                .HasKey(k => new { k.ClienteFK, k.BebidaFK });
+        }
+    }
+}
